74e2a4ab18e3951c927c61978a9a4b5395353893
Juho Rinta-Paavola <juhorp@juhorp.fi>
2016-11-09 22:56:06 +0200

    Implement a basic grid

--- src/App.js
+++ src/App.js
@@ -1,21 +1,65 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  render() {
+function Square(props) {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+ 	<button className="Square" onClick={() => props.onClick()}>
+ 	    {props.value}
+ 	</button>
     );
-  }
+}
+
+const BOARD_WIDTH=10;
+const BOARD_HEIGHT=8;
+
+class Board extends Component {
+    constructor() {
+	super();
+	
+	this.state = {
+	    // N.B. the Array contains rows, i.e. indexing is squares[y][x]
+	    squares: Array(BOARD_HEIGHT).fill(null),
+	}
+    }
+    componentWillMount() {
+	const squares = this.state.squares.slice();
+	for (var i = 0; i < BOARD_HEIGHT; i++) {
+	    squares[i] = Array(BOARD_WIDTH).fill("0");
+	}
+	this.setState({ squares: squares })
+    }
+    renderSquare(x, y) {
+	return <Square key={y * BOARD_WIDTH + x} value={this.state.squares[y][x]} onClick={() => this.handleClick(x, y)} />;
+    }
+    render() {
+	// Render the squares
+	const board = this.state.squares.map((row, y) => {
+	    const squares = row.map((square, x) => {
+		return this.renderSquare(x, y);
+	    });
+	    // A row is a div containing all the squares
+	    return (
+		<div key={y} className="board-row">
+		    {squares}
+		</div>
+	    );
+	});
+	return (
+	    <div className="gameBoard">
+	        {board}
+	    </div>
+	);
+    }
+}
+
+class App extends Component {
+    render() {
+	return (
+	    <div className="game">
+		<Board />
+	    </div>
+	);
+    }
 }
 
 export default App;
